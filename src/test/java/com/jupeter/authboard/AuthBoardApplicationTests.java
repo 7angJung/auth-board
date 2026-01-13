e1e9d20c4e3eb07e4a40d52cdbcf0ce7d9ec84b3
7angJung <peterj0117@naver.com>
2026-01-13 12:23:39 +0900

    chore: init Spring Boot project

---
+++ src/test/java/com/jupeter/authboard/AuthBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupeter.authboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
